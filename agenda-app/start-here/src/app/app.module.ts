45f86d80a5fdb96b082beef3618e27d976bd0070
Duvian <d.restrepo1@pascualbravo.edu.co>
2019-11-15 10:02:24 -0500

    se crearo el componente event  y se importo y renderizo el EventComponent

--- agenda-app/start-here/src/app/app.module.ts
+++ agenda-app/start-here/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
-
+import { EventComponent } from './components/event/event.component'
 
 @NgModule({
   declarations: [
     AppComponent,
+    EventComponent
   ],
   imports: [
     BrowserModule,
@@ -16,6 +17,6 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
   ],
   providers: [],
   bootstrap: [AppComponent],
-  schemas: [ NO_ERRORS_SCHEMA ]
+  schemas: [NO_ERRORS_SCHEMA]
 })
 export class AppModule { }
